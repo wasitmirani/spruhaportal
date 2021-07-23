05c24e83cb2ea7bf546d3c5de3d874936d3f23af
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-07-23 11:00:07 +0500

    update auth page

---
+++ resources/views/layouts/backend/authmaster.blade.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="utf-8">
+		<meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+		<meta name="description" content="Spruha -  Admin Panel laravel Dashboard Template">
+		<meta name="author" content="Spruko Technologies Private Limited">
+		<meta name="keywords" content="admin laravel template, template laravel admin, laravel css template, best admin template for laravel, laravel blade admin template, template admin laravel, laravel admin template bootstrap 4, laravel bootstrap 4 admin template, laravel admin bootstrap 4, admin template bootstrap 4 laravel, bootstrap 4 laravel admin template, bootstrap 4 admin template laravel, laravel bootstrap 4 template, bootstrap blade template, laravel bootstrap admin template">
+
+		<!-- Favicon -->
+		<link rel="icon" href="assets/img/brand/favicon.ico" type="image/x-icon"/>
+
+		<!-- Title -->
+		<title>{{config('app.name')}}- AUTH</title>
+
+		<!-- Bootstrap css-->
+		<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Icons css-->
+		<link href="assets/plugins/web-fonts/icons.css" rel="stylesheet"/>
+		<link href="assets/plugins/web-fonts/font-awesome/font-awesome.min.css" rel="stylesheet">
+		<link href="assets/plugins/web-fonts/plugin.css" rel="stylesheet"/>
+
+		<!-- Style css-->
+		<link href="assets/css/style.css" rel="stylesheet">
+		<link href="assets/css/skins.css" rel="stylesheet">
+		<link href="assets/css/dark-style.css" rel="stylesheet">
+		<link href="assets/css/colors/default.css" rel="stylesheet">
+
+
+		<!-- Color css-->
+		<link id="theme" rel="stylesheet" type="text/css" media="all" href="assets/css/colors/color.css">
+
+		<!-- Switcher css-->
+		<link href="assets/switcher/css/switcher.css" rel="stylesheet">
+		<link href="assets/switcher/demo.css" rel="stylesheet">
+
+
+
+	</head>
+
+	<body class="main-body leftmenu">
+
+			<!-- Switcher -->
+
+		<!-- End Switcher -->
+
+
+
+		<!-- Page -->
+		<div class="page main-signin-wrapper">
+
+			<!-- Row -->
+                @yield('content')
+			<!-- End Row -->
+
+		</div>
+		<!-- End Page -->
+
+		<!-- Jquery js-->
+		<script src="assets/plugins/jquery/jquery.min.js"></script>
+
+		<!-- Bootstrap js-->
+		<script src="assets/plugins/bootstrap/js/popper.min.js"></script>
+		<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+		<!-- Select2 js-->
+		<script src="assets/plugins/select2/js/select2.min.js"></script>
+
+
+		<!-- Custom js -->
+		<script src="assets/js/custom.js"></script>
+
+		<!-- Switcher js -->
+		<script src="assets/switcher/js/switcher.js"></script>
+
+
+
+
+	</body>
+</html>
--- resources/views/layouts/backend/master.blade.php
+++ resources/views/layouts/backend/master.blade.php
@@ -49,116 +49,13 @@
 	<body class="main-body leftmenu">
 
 		<!-- Switcher -->
-		<div class="switcher-wrapper">
-			<div class="demo_changer">
-				<div class="demo-icon bg_dark">
-					<i class="fa fa-cog fa-spin  text_primary"></i>
-				</div>
-				<div class="form_holder sidebar-right1">
-					<div class="row">
-						<div class="predefined_styles">
-							<div class="swichermainleft text-center">
-								<div class="p-3">
-									<a href="../index.html" class="btn btn-primary btn-block mt-0">View Demo</a>
-									<a href="https://themeforest.net/item/spruha-laravel-admin-dashboard-template/29818213" class="btn btn-secondary btn-block">Buy Now</a>
-									<a href="https://themeforest.net/user/spruko/portfolio" class="btn btn-info btn-block">Our Portfolio</a>
-								</div>
-							</div>
-							<div class="swichermainleft">
-							<h4>Navigation Style</h4>
-								<div class="p-3">
-									<a href="index" class="btn btn-primary btn-block mt-0">LTR VERSION</a>
-									<a href="https://laravel.spruko.com/spruha/rtl/index" class="btn btn-success btn-block">RTL VERSION</a>
-								</div>
-							</div>
-							<div class="swichermainleft">
-							<h4>Layout Style</h4>
-								<div class="p-3">
-									<a href="index" class="btn btn-primary btn-block mt-0">Left Menu</a>
-									<a href="horizontal" class="btn btn-success btn-block">Horizontal Menu</a>
-								</div>
-							</div>
-							<div class="swichermainleft">
-								<h4 class="font-bold text-sm mr-3">Default Theme Switcher</h4>
-								<div class="swichermainleft my-4">
-									<a class="wscolorcode red-btn color blackborder color1" href="#" data-theme="assets/css/colors/color1.css"></a>
-									<a class="wscolorcode purple-btn color blackborder color2" href="#" data-theme="assets/css/colors/color2.css"></a>
-									<a class="wscolorcode green-btn color blackborder color3" href="#" data-theme="assets/css/colors/color3.css"></a>
-									<a class="wscolorcode pink-btn color blackborder color4" href="#" data-theme="assets/css/colors/color4.css"></a>
-									<a class="wscolorcode orange-btn color blackborder color5" href="#" data-theme="assets/css/colors/color5.css"></a>
-								</div>
-							</div>
-							<div class="swichermainleft">
-								<h4>Ions Styles</h4>
-								<div class="switch_section my-2">
-									<div class="switch-toggle d-flex">
-										<span class="mr-auto">Icon Style</span>
-										<div class="onoffswitch2">
-											<input type="checkbox" name="onoffswitch2" id="myonoffswitch51" class="onoffswitch2-checkbox">
-											<label for="myonoffswitch51" class="onoffswitch2-label"></label>
-										</div>
-									</div>
-								</div>
-								<div class="switch_section my-2">
-									<div class="switch-toggle d-flex">
-										<span class="mr-auto">Theme Style</span>
-										<div class="onoffswitch2">
-											<input type="checkbox" name="onoffswitch2" id="myonoffswitch52" class="onoffswitch2-checkbox">
-											<label for="myonoffswitch52" class="onoffswitch2-label"></label>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="swichermainleft">
-								<h4>Theme Layout</h4>
-								<div class="switch_section d-flex my-4">
-									<div class="d-block text-center mx-auto">
-										<button type="button" id="background5" class="bg5 mb-3 wscolorcode1 blackborder"></button>
-										<span class="badge badge-light tx-12">Dark layout</span>
-									</div>
-									<div class="d-block text-center mx-auto">
-										<button type="button" id="background6" class="bg6 mb-3 wscolorcode1 blackborder"></button>
-										<span class="badge badge-light tx-12">Light layout</span>
-									</div>
-								</div>
-							</div>
-							<div class="swichermainleft">
-								<h4>Header Styles Mode</h4>
-								<div class="switch_section d-flex my-4">
-									<div class="d-block text-center mx-auto">
-										<button type="button" id="background3" class="bg3 mb-3 wscolorcode1 blackborder"></button>
-										<span class="badge badge-light tx-12">Dark Header</span>
-									</div>
-									<div class="d-block text-center mx-auto">
-										<button type="button" id="background4" class="bg4 mb-3 wscolorcode1 blackborder"></button>
-										<span class="badge badge-light tx-12">Color Header</span>
-									</div>
-								</div>
-							</div>
-							<div class="swichermainleft">
-								<h4>Leftmenu Styles Mode</h4>
-								<div class="switch_section d-flex my-4">
-									<div class="d-block text-center mx-auto">
-										<button type="button" id="background1" class="bg1 wscolorcode1 blackborder"></button>
-										<span class="badge badge-light tx-12">Color Menu</span>
-									</div>
-									<div class="d-block text-center mx-auto">
-										<button type="button" id="background2" class="bg2 wscolorcode1 blackborder"></button>
-										<span class="badge badge-light tx-12">Light Menu</span>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+
 		<!-- End Switcher -->
 
 		<!-- Loader -->
-		<div id="global-loader">
+		{{-- <div id="global-loader">
 			<img src="assets/img/loader.svg" class="loader-img" alt="Loader">
-		</div>
+		</div> --}}
         <!-- End Loader -->
 
 		<!-- Page -->
@@ -177,14 +74,14 @@
 				<div class="main-sidebar-body">
 					<ul class="nav">
 						<li class="nav-header"><span class="nav-label">Dashboard</span></li>
-						<li class="nav-item">
+						<li class="nav-item active">
 							<a class="nav-link" href="index"><span class="shape1"></span><span class="shape2"></span><i class="ti-home sidemenu-icon"></i><span class="sidemenu-label">Dashboard</span></a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link with-sub" href="#"><span class="shape1"></span><span class="shape2"></span><i class="ti-wallet sidemenu-icon"></i><span class="sidemenu-label">Crypto Currencies</span><i class="angle fe fe-chevron-right"></i></a>
 							<ul class="nav-sub">
-								<li class="nav-sub-item">
-									<a class="nav-sub-link" href="cryptodashbaord">Dashboard</a>
+								<li class="nav-sub-item ">
+									<a class="nav-sub-link " href="cryptodashbaord">Dashboard</a>
 								</li>
 								<li class="nav-sub-item">
 									<a class="nav-sub-link" href="cryptomarket">Marketcap</a>
