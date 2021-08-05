a7152e1cf4859d553c42551ce91c4bdcdefc40e2
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-08-05 19:53:08 +0500

    links

--- resources/views/layouts/backend/master.blade.php
+++ resources/views/layouts/backend/master.blade.php
@@ -32,8 +32,6 @@
 		<!-- Color css-->
 		<link id="theme" rel="stylesheet" type="text/css" media="all" href="assets/css/colors/color.css">
 
-		<!-- Select2 css-->
-        <link href="assets/plugins/select2/css/select2.min.css" rel="stylesheet">
 
 				<!-- Mutipleselect css-->
 		<link rel="stylesheet" href="assets/plugins/multipleselect/multiple-select.css">
@@ -1576,48 +1574,47 @@
 		<a href="#top" id="back-to-top"><i class="fe fe-arrow-up"></i></a>
 
 		<!-- Jquery js-->
-		<script src="assets/plugins/jquery/jquery.min.js"></script>
+		<script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
 
 		<!-- Bootstrap js-->
-		<script src="assets/plugins/bootstrap/js/popper.min.js"></script>
-        <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+		<script src="{{asset('assets/plugins/bootstrap/js/popper.min.js')}}"></script>
+        <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
 
 		<!-- Select2 js-->
-		<script src="assets/plugins/select2/js/select2.min.js"></script>
 
 		<!-- Perfect-scrollbar js -->
-		<script src="assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="{{asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
 
 		<!-- Sidemenu js -->
-		<script src="assets/plugins/sidemenu/sidemenu.js"></script>
+		<script src="{{asset('assets/plugins/sidemenu/sidemenu.js')}}"></script>
 
 		<!-- Sidebar js -->
-		<script src="assets/plugins/sidebar/sidebar.js"></script>
+		<script src="{{asset('assets/plugins/sidebar/sidebar.js')}}"></script>
 
 		        <!-- Internal Chart.Bundle js-->
-		<script src="assets/plugins/chart.js/Chart.bundle.min.js"></script>
+		<script src="{{asset('assets/plugins/chart.js')}}/Chart.bundle.min.js')}}"></script>
 
 		<!-- Peity js-->
-        <script src="assets/plugins/peity/jquery.peity.min.js"></script>
+        <script src="{{asset('assets/plugins/peity/jquery.peity.min.js')}}"></script>
 
 		<!-- Internal Morris js -->
-		<script src="assets/plugins/raphael/raphael.min.js"></script>
-		<script src="assets/plugins/morris.js/morris.min.js"></script>
+		<script src="{{asset('assets/plugins/raphael/raphael.min.js')}}"></script>
+		<script src="{{asset('assets/plugins/morris.js')}}/morris.min.js')}}"></script>
 
 		<!-- Circle Progress js-->
-		<script src="assets/js/circle-progress.min.js"></script>
-		<script src="assets/js/chart-circle.js"></script>
+		<script src="{{asset('assets/js/circle-progress.min.js')}}"></script>
+		<script src="{{asset('assets/js/chart-circle.js')}}"></script>
 
 		<!-- Internal Dashboard js-->
-        <script src="assets/js/index.js"></script>
+        <script src="{{asset('assets/js/index.js')}}"></script>
 
 		<!-- Sticky js -->
-		<script src="assets/js/sticky.js"></script>
+		<script src="{{asset('assets/js/sticky.js')}}"></script>
 
 		<!-- Custom js -->
-		<script src="assets/js/custom.js"></script>
+		<script src="{{asset('assets/js/custom.js')}}"></script>
 
 		<!-- Switcher js -->
-		<script src="assets/switcher/js/switcher.js"></script>
+		<script src="{{asset('assets/switcher/js/switcher.js')}}"></script>
 	</body>
 </html>
